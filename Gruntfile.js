3c26c008386fd8f56983c43a7b3443297f5ccf8e
kissyteam <tiehang.lth@taobao.com>
2015-02-24 02:31:18 +0800

    sync daily branch 2.0.1

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,21 @@
 module.exports = function(grunt) {
+	var task = grunt.task;
+    var SRC = './';
     grunt.initConfig({
         // 配置文件，参考package.json配置方式，必须设置项是
         // name, version, author
         // name作为gallery发布后的模块名
         // version是版本，也是发布目录
         // author必须是{name: "xxx", email: "xxx"}格式
-        pkg: grunt.file.readJSON('abc.json'),
+        pkg: grunt.file.readJSON('package.json'),
         banner: '/*!build time : <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>*/\n',
 
+        // 对build目录进行清理
+        clean: {
+            build: {
+                src: './build/*'
+			}
+        },
         // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
         // 添加
         kmc: {
@@ -18,13 +26,17 @@ module.exports = function(grunt) {
                         path: '../'
                     }
                 ],
-                map: [["<%= pkg.name %>/", "gallery/<%= pkg.name %>/"]]
+                depFilePath: 'mods.js',
+                fixModuleName:true,
+                map: [["<%= pkg.name %>/", "kg/<%= pkg.name %>/<%= pkg.version %>/"]]
             },
             main: {
                 files: [
                     {
-                        src: "<%= pkg.version %>/index.js",
-                        dest: "<%= pkg.version %>/build/index.js"
+                        expand: true,
+                        cwd: SRC,
+                        src: [ './*.js','!./Gruntfile.js' ],
+                        dest: 'build/'
                     }
                 ]
             }
@@ -35,6 +47,11 @@ module.exports = function(grunt) {
          */
         uglify: {
             options: {
+                compress:{
+                    global_defs:{"DEBUG":false},
+                    drop_console:true,
+                    dead_code:true
+                },
                 banner: '<%= banner %>',
                 beautify: {
                     ascii_only: true
@@ -44,34 +61,92 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        cwd: '<%= pkg.version %>/build',
+                        cwd: './build',
                         src: ['**/*.js', '!**/*-min.js'],
-                        dest: '<%= pkg.version %>/build',
+                        dest: './build',
                         ext: '-min.js'
                     }
                 ]
             }
         },
-        copy: {
+        less: {
+            options: {
+                paths: './'
+            },
             main: {
                 files: [
                     {
                         expand: true,
-                        cwd: '<%= pkg.version %>',
-                        src: ['**/*.css'],
-                        dest: '<%= pkg.version %>/build'
+						cwd:SRC,
+                        src: ['**/*.less',
+                            '!node_modules/**/*.less',
+							'!build/**/*.less',   
+							'!demo/**/*.less'],
+                        dest: './build/',
+                        ext: '.css'
                     }
                 ]
             }
         },
+		// 拷贝 CSS 文件
+		copy : {
+			main: {
+				files:[
+					{
+						expand:true,
+						cwd:SRC,
+						src: [
+							'**/*.css',
+                            '!node_modules/**/*.css',
+							'!build/**/*.css',
+							'!demo/**/*.css'
+						], 
+						dest: './build/', 
+						filter: 'isFile'
+					}
+				]
+			}
+		},
+		// 监听JS、CSS、LESS文件的修改
+        watch: {
+            'all': {
+                files: [
+					'./src/**/*.css',
+					'!./build/**/*'
+				],
+                tasks: [ 'build' ]
+            }
+		},
         cssmin: {
+            scss: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './build',
+                        src: ['**/*.scss.css', '!**/*.scss-min.css'],
+                        dest: './build',
+                        ext: '.scss-min.css'
+                    }
+                ]
+            },
+            less: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: './build',
+                        src: ['**/*.less.css', '!**/*.less-min.css'],
+                        dest: './build',
+                        ext: '.less-min.css'
+                    }
+                ]
+            },
             main: {
                 files: [
                     {
                         expand: true,
-                        cwd: '<%= pkg.version %>/build',
-                        src: ['**/*.css', '!**/*-min.css'],
-                        dest: '<%= pkg.version %>/build',
+                        cwd: './build',
+                        src: ['**/*.css', '!**/*-min.css','!**/*.less.css','!**/*.scss.css'],
+                        dest: './build',
                         ext: '-min.css'
                     }
                 ]
@@ -82,7 +157,20 @@ module.exports = function(grunt) {
     // 使用到的任务，可以增加其他任务
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-kmc');
+    grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    return grunt.registerTask('default', ['kmc', 'uglify','copy','cssmin']);
+    grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+
+	grunt.registerTask('build', '默认构建任务', function() {
+		task.run(['clean:build', 'kmc','uglify', 'copy','less','cssmin']);
+	});
+
+    return grunt.registerTask('default', '',function(type){
+		if (!type) {
+			task.run(['build']);
+		}
+	});
 };
--- doc/meta/alias.js
+++
@@ -1,3 +0,0 @@
-config({
-    'kg/tb-video-player/index': {alias: ['kg/tb-video-player/2.0.0/index']}
-});
